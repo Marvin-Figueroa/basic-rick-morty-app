cc007ade1dc8e31fa9ef3fc29dd0b9ce9407579d
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-07-09 16:03:06 -0600

    Create .env file to store variables

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 export default axios.create({
-  baseURL: 'https://rickandmortyapi.com/api'
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 export { AxiosError };
