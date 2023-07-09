38292a394df3bd75bb7925a8eecce8c9a27b200e
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-07-09 11:11:27 -0600

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Heading } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-  return <Heading color='blueviolet'>Rick & Morty</Heading>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav"
+      "sidebar main"`
+      }}>
+      <GridItem bg='gold' area={'nav'}>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        <GridItem bg='blueviolet' area={'sidebar'}>
+          Sidebar
+        </GridItem>
+      </Show>
+      <GridItem bg='tomato' area={'main'}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
