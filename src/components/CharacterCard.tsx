2f05d48813407cf2546e6d9f5c8e2d9667e873d5
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-07-10 11:30:32 -0600

    Add character status and gender icons

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,5 +1,30 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { BsHeartPulseFill, BsQuestionCircle } from 'react-icons/bs';
+import { FaSkull } from 'react-icons/fa';
+import { PiGenderMaleFill, PiGenderFemaleFill } from 'react-icons/pi';
+import {
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  Text,
+  Tooltip
+} from '@chakra-ui/react';
 import { Character } from '../types';
+import { IconType } from 'react-icons';
+
+const statusMap: { [key: string]: IconType } = {
+  alive: BsHeartPulseFill as IconType,
+  dead: FaSkull as IconType,
+  unknown: BsQuestionCircle as IconType
+};
+
+const genderMap: { [key: string]: IconType } = {
+  male: PiGenderMaleFill as IconType,
+  female: PiGenderFemaleFill as IconType,
+  unknown: BsQuestionCircle as IconType
+};
 
 interface Props {
   character: Character;
@@ -9,10 +34,40 @@ const CharacterCard = ({ character }: Props) => {
   return (
     <Card borderRadius='lg' overflow='hidden' boxShadow='lg'>
       <Image src={character.image} />
-      <CardBody>
+      <CardBody display='flex' flexDirection='column' gap='15px'>
         <Heading as='h2' size='md'>
           {character.name}
         </Heading>
+        <Text>{character.species}</Text>
+        <HStack
+          marginTop='auto'
+          alignItems='center'
+          justifyContent='space-between'>
+          <Tooltip label={character.status} fontSize='md'>
+            <span>
+              <Icon
+                boxSize='30px'
+                color={
+                  character.status === 'Dead'
+                    ? 'red.500'
+                    : character.status === 'unknown'
+                    ? 'gray.500'
+                    : 'green.500'
+                }
+                as={statusMap[character.status?.toLowerCase()]}
+              />
+            </span>
+          </Tooltip>
+          <Tooltip label={character.gender} fontSize='md'>
+            <span>
+              <Icon
+                color={character.gender === 'Female' ? 'pink.500' : 'blue.500'}
+                boxSize='30px'
+                as={genderMap[character.gender?.toLowerCase()]}
+              />
+            </span>
+          </Tooltip>
+        </HStack>
       </CardBody>
     </Card>
   );
